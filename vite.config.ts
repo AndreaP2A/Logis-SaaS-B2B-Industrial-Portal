f03a9c1115c52812f92ce0966e293d1605c22663
AndreaP2A <andrea.porche2a@gmail.com>
2026-01-06 13:56:53 +0000

    chore: setup github actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // Set base to your repository name for GitHub Pages: base: '/your-repo-name/'
-  base: '/logis-b2b-portal/',
+  base: '/Logis-SaaS-B2B-Industrial-Portal/',
   plugins: [
     react(),
     tailwindcss(),
